1ff465603c4f0629e9b9e054e03515fdedcb5b85
23Lapial <59451344+23Lapial@users.noreply.github.com>
2020-02-27 10:28:02 -0500

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,3 +1,5 @@
 #!/usr/bin/env python3
 
-print(''.join(sorted([character.upper() if index % 2 == 0 else character.lower() for index, character in enumerate(input().lower())], reverse=True)))
+print(''.join(sorted(
+  [character.upper() if index % 2 == 0 else character.lower() for index, character in enumerate(input().lower())],
+  reverse=True)))
